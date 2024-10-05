2a633b6ab8e22358b3ce2dde10130c6983f80b30
Natsuhiko Mizutani <mizutani.natsu@gmail.com>
2024-10-05 14:14:17 +0900

    [Cleaned] Removed formatter generated differences,.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,9 +2,11 @@ import unittest
 
 
 class BasicTest(unittest.TestCase):
+
     def test_import(self):
+
         from embree import embree
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
